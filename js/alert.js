88e214fd19fff6252cfcfc9f990dfbfabb0022ef
Andrii Babenko <andriibvw@gmail.com>
2022-08-13 15:10:53 +0300

    refactor: remove strict mode

--- js/alert.js
+++ js/alert.js
@@ -1,17 +1,15 @@
 window.addEventListener('DOMContentLoaded', () => {
-  'use strict';
-
   const btn = document.querySelector('.alarm-btn');
   const box = document.querySelector('.alarm-box');
   const boxText = document.querySelector('.alarm-box__text');
 
   boxText.textContent = "This and other portfolio pages are demo";
-  
+
   if (localStorage.hasOwnProperty('timeAction')) {
     const newTimeAction = new Date().getTime();
     const timeAction = JSON.parse(localStorage.getItem('timeAction'));
     const timer = newTimeAction - timeAction;
-  
+
     if (timer > 172800000) {
       localStorage.clear();
       showAlarm();
@@ -23,11 +21,11 @@ window.addEventListener('DOMContentLoaded', () => {
   function showAlarm() {
     box.style.display = 'block';
     btn.addEventListener('click', () => {
-      
+
       box.classList.add('alarm-box_closed');
       localStorage.setItem('alarm', true);
       localStorage.setItem('timeAction', JSON.stringify(new Date().getTime()));
-      
+
       setTimeout(() => {
         box.style.display = 'none';
       }, 600);
